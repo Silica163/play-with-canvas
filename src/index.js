97ec3122c37d12a9f5f6ddbf92d4c4d1b93e482f
Silica163 <96188282+Silica163@users.noreply.github.com>
2022-09-13 21:57:32 +0700

    add radar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const ctx1 = document.getElementById("c1").getContext("2d");
 const ctx2 = document.getElementById("c2").getContext("2d");
+const ctx3 = document.getElementById("c3").getContext("2d");
 
 function genFiboNum(t){
 	var fibo = [] ,n=1,a=1,b=1,c=0;
@@ -12,5 +13,73 @@ function genFiboNum(t){
 	}
 	return fibo;
 }
+var fibo = genFiboNum(1000)
 
-console.log(genFiboNum(100))
+function drawFibo(ctx){
+	for(let pos = 0,c = 0,an=0;pos <= ctx.canvas.width;pos+=3,c++,an+=0.01){
+		ctx.lineWidth=1;
+		ctx.beginPath();
+		ctx.moveTo(ctx.canvas.width/2,ctx.canvas.height/2);
+		ctx.lineTo(0,fibo[c]);
+		ctx.rotate(an);
+		if(an >= 6.3)an = 0;
+		ctx.stroke();
+	}
+}
+
+drawFibo(ctx1);
+
+function radar(ctx){
+	var w = ctx.canvas.width,h = ctx.canvas.height,int,an=0,posList = [];
+	function drawRay(){
+		drawGrid(ctx,50,50);
+		ctx.beginPath();
+		ctx.strokeStyle = "#0f0";
+		ctx.lineWidth = 2
+		ctx.moveTo(w/2,h/2);
+		ctx.lineTo((w/2)+(Math.cos(an)*(w/2)),(h/2)+(Math.sin(an)*(h/2)));
+		ctx.moveTo(w,h/2);
+		ctx.arc(w/2,h/2,h/2,0,Math.PI*2,true);
+		ctx.stroke();
+		ctx.fillStyle = "#0002";
+		ctx.fillRect(0,0,w,h);
+		if(an >= 360)clearInterval(int);
+		an+=0.01;
+	}
+	int = setInterval(drawRay,0)
+}
+
+window.requestAnimationFrame(radar.bind(this,ctx2));
+
+function bwGrid(ctx){
+//	console.log(ctx.canvas);
+	let x =0 ,y= 0,int;
+	function drawRectX(){
+		ctx.beginPath();
+//		console.log(x,y);
+		if(x%2 == y%2)ctx.fillRect(x,y,1,1);
+		if(x <= ctx.canvas.width)x++
+			else if(y <= ctx.canvas.height)x=0,y++;
+		if(x==ctx.canvas.width && y == ctx.canvas.height)clearInterval(int);
+		setTimeout(drawRectX,0);
+	}
+	int = setInterval(drawRectX,0);
+}
+
+//window.requestAnimationFrame(bwGrid.bind(this,ctx3));
+
+
+function drawGrid(ctx,w=10,h=10){
+	ctx.beginPath();
+	ctx.strokeStyle = '#080';
+	ctx.lineWidth='1px';
+	for(let x = 0 ;x<= ctx.canvas.width;x+=w){
+		ctx.moveTo(0,x);
+		ctx.lineTo(ctx.canvas.width,x);
+	}
+	for(let y=0;y<=ctx.canvas.height;y+=h){
+		ctx.moveTo(y,0);
+		ctx.lineTo(y,ctx.canvas.height);
+	}
+	ctx.stroke();
+}
